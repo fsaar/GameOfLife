82aaa53da49e0fabef03468c50eef26bcd0ba128
fsaar <frank.saar@gmail.com>
2024-01-23 20:27:14 +0000

    Initial Commit

---
+++ GameOfLife/GameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLifeApp.swift
+//  GameOfLife
+//
+//  Created by Frank Saar on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
